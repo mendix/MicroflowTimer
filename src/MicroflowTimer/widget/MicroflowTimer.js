3fafcd4ec8ea02c05c4800d97e0d28b7603eb06d
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2016-02-18 11:46:34 +0100

    Cleanup widget code

--- src/MicroflowTimer/widget/MicroflowTimer.js
+++ src/MicroflowTimer/widget/MicroflowTimer.js
@@ -1,17 +1,12 @@
-/*jslint white:true, nomen: true, plusplus: true */
-/*global mx, define, require, browser, devel, console */
-/*mendix */
-
-// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
 require([
-    'dojo/_base/declare', 'mxui/widget/_WidgetBase',
-    'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text'
-], function (declare, _WidgetBase, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, text) {
-    'use strict';
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+     "dojo/_base/lang"
+], function (declare, _WidgetBase, lang) {
+    "use strict";
 
-    // Declare widget's prototype.
-    return declare('MicroflowTimer.widget.MicroflowTimer', [_WidgetBase], {
-
+    // Declare widget"s prototype.
+    return declare("MicroflowTimer.widget.MicroflowTimer", [_WidgetBase], {
 
         // Parameters configured in the Modeler.
         interval: 30000,
@@ -23,17 +18,11 @@ require([
         _handle: null,
         _contextObj: null,
         _timer: null,
-        _blocked : null,
+        _timeout: null,
         _timerStarted: false,
 
-        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
-        postCreate: function () {
-            console.log(this.id + '.postCreate');
-        },
-
-        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
         update: function (obj, callback) {
-            console.log(this.id + '.update');
+            logger.debug(this.id + ".update");
 
             this._contextObj = obj;
             if (!this._timerStarted) {
@@ -43,36 +32,21 @@ require([
             callback();
         },
 
-        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
-        enable: function () {
+        resize: function (box) {},
 
-        },
-
-        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
-        disable: function () {
-
-        },
-
-        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
-        resize: function (box) {
-
-        },
-
-        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
         uninitialize: function () {
-            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
             this._stopTimer();
         },
 
         _runTimer: function () {
-            console.log(this.id + '._runTimer', this.interval);
+            logger.debug(this.id + "._runTimer", this.interval);
             if (this.microflow !== "" && this._contextObj) {
                 this._timerStarted = true;
                 if (this.once) {
-                    setTimeout(lang.hitch(this, this._execMf), this.interval);
+                    this._timeout = setTimeout(lang.hitch(this, this._execMf), this.interval);
                 } else {
                     if (this.startatonce) {
-                        this._execMf([this._contextObj.getGuid()], this.microflow);
+                        this._execMf();
                     }
                     this._timer = setInterval(lang.hitch(this, this._execMf), this.interval);
                 }
@@ -80,39 +54,43 @@ require([
         },
 
         _stopTimer: function () {
-            console.log(this.id + '._stopTimer');
+            logger.debug(this.id + "._stopTimer");
             if (this._timer !== null) {
+                logger.debug(this.id + "._stopTimer timer cleared");
                 clearInterval(this._timer);
                 this._timer = null;
+            }
+            if (this._timeout !== null) {
+                logger.debug(this.id + "._stopTimer timeout cleared");
+                clearTimeout(this._timeout);
+                this._timeout = null;
             }
         },
 
         _execMf: function () {
-            console.log(this.id + '._execMf');
+            logger.debug(this.id + "._execMf");
 
-            var self = this,
-                guids = [this._contextObj.getGuid()],
-                mf = this.microflow;
-                mx.data.action({
-                    params: {
-                        applyto: "selection",
-                        actionname: mf,
-                        guids: guids
-                    },
-                    store: {
-                        caller: this.mxform
-                    },
-                    callback: function (result) {
-                        if (!result) {
-                            console.log('stopping timer');
-                            self._stopTimer();
-                            //this._blocked = false;
-                        }
-                    },
-                    error: function (error) {
-                        console.warn('Error executing mf: ', error);
-                    }
-                });
+            if (this._contextObj && this.microflow !== "") {
+              mx.data.action({
+                  params: {
+                      applyto: "selection",
+                      actionname: this.microflow,
+                      guids: [this._contextObj.getGuid()]
+                  },
+                  store: {
+                      caller: this.mxform
+                  },
+                  callback: lang.hitch(this, function (result) {
+                      if (!result) {
+                          logger.debug(this.id + "._execMf callback, stopping timer");
+                          this._stopTimer();
+                      }
+                  }),
+                  error: function (error) {
+                      console.warn("Error executing mf: ", error);
+                  }
+              });
+            }
         }
     });
 });
