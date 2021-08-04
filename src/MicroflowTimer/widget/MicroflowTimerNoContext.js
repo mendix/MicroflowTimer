2dc7882e668d393cf40802c26fbd6819ca1b3449
Eric Tieniber <tieniber@gmail.com>
2021-08-04 13:43:26 -0400

    add no context version

---
+++ src/MicroflowTimer/widget/MicroflowTimerNoContext.js
@@ -0,0 +1,8 @@
+define([
+    "dojo/_base/declare", "./MicroflowTimer"
+], function (declare, MicroflowTimerNoContext) {
+    "use strict";
+
+    // Declare widget"s prototype.
+    return declare("MicroflowTimer.widget.MicroflowTimer", [MicroflowTimerNoContext]);
+});
